f6df1827c6577e72c134f551ac14c6136c42fdde
lidongshan <lidongshan@goodix.com>
2022-02-14 17:02:26 +0800

    add vendor base code
    Signed-off-by: zounan <zounan@goodix.com>

--- gr5515_sk_xts_demo/tests/drivers/gpio_test.c
+++ gr5515_sk_xts_demo/tests/drivers/gpio_test.c
@@ -16,29 +16,31 @@
 #include "gpio_if.h"
 #include "driver_test.h"
 
-#define GPIO_LED1         4
-#define GPIO_LED2         44
-#define GPIO_BUTTON_UP    12
-#define GPIO_BUTTON_DOWN  13
-#define GPIO_BUTTON_LEFT  14
-#define GPIO_BUTTON_RIGH  15
+static uint16_t gpio_led1 = 4;
+static uint16_t gpio_led2 = 44;
+static uint16_t gpio_button_up   = 12;
+static uint16_t gpio_button_down = 13;
+static uint16_t gpio_button_left = 14;
+static uint16_t gpio_button_righ = 15;
+static uint16_t gpio_irq_state = 0;
 
 static void gpio_output_test(void)
 {
     static int cnt = 0;
 
-    LOG_I("set gpio %d output\r\n", GPIO_LED1);
-    GpioSetDir(GPIO_LED1, GPIO_DIR_OUT);
-    GpioSetDir(GPIO_LED2, GPIO_DIR_OUT);
+    LOG_I("set gpio %d output\r\n", gpio_led1);
+    GpioSetDir(gpio_led1, GPIO_DIR_OUT);
+    GpioSetDir(gpio_led2, GPIO_DIR_OUT);
 
-    if (cnt++ % 2) {
+    if (cnt++ % 2)
+    {
         LOG_I("set led on\r\n");
-        GpioWrite(GPIO_LED1, GPIO_VAL_LOW);
-        GpioWrite(GPIO_LED2, GPIO_VAL_HIGH);
-    } else {
+        GpioWrite(gpio_led1, GPIO_VAL_LOW);
+    }
+    else
+    {
         LOG_I("set led of\r\n");
-        GpioWrite(GPIO_LED1, GPIO_VAL_HIGH);
-        GpioWrite(GPIO_LED2, GPIO_VAL_LOW);
+        GpioWrite(gpio_led1, GPIO_VAL_HIGH);
     }
 }
 
@@ -47,11 +49,13 @@ static void gpio_input_test(void)
     int cnt = 0;
     uint16_t val = 1;
 
-    GpioSetDir(GPIO_BUTTON_UP, GPIO_DIR_IN);
+    GpioSetDir(gpio_button_up, GPIO_DIR_IN);
     LOG_I("please push up key: >>> ");
-    while (cnt++ < 10) {
-        GpioRead(GPIO_BUTTON_UP, &val);
-        if (0 == val) {
+    while (cnt++ < 10)
+    {
+        GpioRead(gpio_button_up, &val);
+        if (0 == val)
+        {
             LOG_I("up key\r\n");
             break;
         }
@@ -59,34 +63,49 @@ static void gpio_input_test(void)
         osDelay(100);
     }
 
-    if (val) {
+    if (val)
         LOG_I("none key\r\n");
-    }
 }
 
 static int32_t gpio_irq_handler(uint16_t gpio, void *data)
 {
+    uint16_t val = 0;
+
     LOG_I("%s enter, gpio = %d, data = %d\r\n", __func__, gpio, *(int *)data);
+
+    gpio_irq_state = *(int *)data;
+
     return 0;
 }
 
 static void gpio_irq_test(void)
 {
-    static bool enable = false;
     static int val = 1234;
 
-    if (enable == false) {
-        enable = true;
-        GpioSetDir(GPIO_BUTTON_LEFT, GPIO_DIR_IN);
-        GpioSetIrq(GPIO_BUTTON_LEFT, GPIO_IRQ_TRIGGER_RISING, (GpioIrqFunc)gpio_irq_handler, &val);
-        GpioEnableIrq(GPIO_BUTTON_LEFT);
-        LOG_I("Set key_left (gpio %d) as interrupt function\r\n", GPIO_BUTTON_LEFT);
+    if (0 == gpio_irq_state)
+    {
+        LOG_I("set key_left (gpio %d) input, @%p\r\n", gpio_button_left, gpio_irq_handler);
+        GpioSetDir(gpio_button_left, GPIO_DIR_IN);
+        GpioSetIrq(gpio_button_left, GPIO_IRQ_TRIGGER_RISING, (GpioIrqFunc)gpio_irq_handler, &val);
+        GpioEnableIrq(gpio_button_left);
+
+        gpio_irq_state = 1;
     }
 }
 
 void gpio_test()
 {
     gpio_output_test();
-    gpio_input_test();
-    gpio_irq_test();
+    // gpio_input_test();
+    // gpio_irq_test();
+
+    // if (gpio_irq_state == 1234)
+    // {
+    //     static int led2_state = GPIO_VAL_LOW;
+
+    //     GpioWrite(gpio_led2, (enum GpioValue)led2_state);
+
+    //     gpio_irq_state = 1;
+    //     led2_state = !led2_state;
+    // }
 }
