70ef67972f7584e2e71ccd3ad8e63a8209aebbec
zhaoxingyu <zhaoxingyu@goodix.com>
2024-06-18 20:54:41 +0800

    Update GR551x SDK to v2.0.2
    Signed-off-by: zhaoxingyu <zhaoxingyu@goodix.com>

--- gr5515_sk_iotlink_demo/ble_template/example.c
+++ gr5515_sk_iotlink_demo/ble_template/example.c
@@ -15,12 +15,10 @@
 
 #include "ohos_init.h"
 #include "cmsis_os2.h"
-#include "gr55xx_sys.h"
-#include "gr55xx_hal_pwr.h"
-#include "scatter_common.h"
-#include "flash_scatter_config.h"
-#include "user_app.h"
-#include "app_callback.h"
+#include "gr_includes.h"
+#include "user_app_xx.h"
+#include "los_task.h"
+#include "app_io.h"
 
 #define BLE_TASK_STACK_SIZE   4096
 #define BLE_TASK_PRIO         25
@@ -29,27 +27,45 @@
 /**@brief Stack global variables for Bluetooth protocol stack. */
 STACK_HEAP_INIT(heaps_table);
 
-static app_callback_t s_app_ble_callback = {
-    .app_ble_init_cmp_callback  = ble_init_cmp_callback,
-    .app_gap_callbacks          = &app_gap_callbacks,
-    .app_gatt_common_callback   = &app_gatt_common_callback,
-    .app_gattc_callback         = &app_gattc_callback,
-    .app_sec_callback           = &app_sec_callback,
-};
-
 static void *BLE_Task(const char *arg)
 {
     (void)arg;
 
+    printf("==========================================\n");
+    printf("===== THIS IS GR551x SDK V202 PROJECT ====\n");
+    printf("==========================================\n");
+
+    const uint8_t test_bd_addr[SYS_BD_ADDR_LEN] = {0x20, 0x15, 0x55, 0x1c, 0x00, 0x92};
+    SYS_SET_BD_ADDR(test_bd_addr);
+
     printf("Initialize the BLE stack.\r\n");
 
     /* init ble stack */
-    ble_stack_init(&s_app_ble_callback, &heaps_table);
+    ble_stack_init(&ble_evt_handler, &heaps_table);
+
+    uint64_t lastTick = LOS_SysCycleGet();
+
+    app_io_init_t io_init = {
+        .pin = APP_IO_PIN_26 | APP_IO_PIN_27 | APP_IO_PIN_17,
+        .mode = APP_IO_MODE_OUTPUT,
+        .mux = APP_IO_MUX_7,
+        .pull = APP_IO_NOPULL,
+    };
+    app_io_init(APP_IO_TYPE_NORMAL, &io_init);
+    app_io_write_pin(APP_IO_TYPE_NORMAL, APP_IO_PIN_26 | APP_IO_PIN_27 | APP_IO_PIN_17, APP_IO_PIN_SET);
 
     while (1) {
-        osDelay(MS_1000);
+        // osDelay(MS_1000);
+        osDelay(1234);
+        app_io_toggle_pin(APP_IO_TYPE_NORMAL, APP_IO_PIN_26);
+        // uint64_t tickElapse = LOS_SysCycleGet() - lastTick;
+        // float msElapse = tickElapse / 64000.f;
+        // printf("Timed Task Elapse: %.5fms\n", msElapse);
+        // lastTick = LOS_SysCycleGet();
     }
 }
+
+LosTaskCB *g_bleTcb = NULL;
 
 void BLE_TaskEntry(void)
 {
@@ -63,9 +79,13 @@ void BLE_TaskEntry(void)
     attr.stack_size = BLE_TASK_STACK_SIZE;
     attr.priority   = BLE_TASK_PRIO;
 
-    if (osThreadNew((osThreadFunc_t)BLE_Task, NULL, &attr) == NULL) {
-        printf("[HelloDemo] Failed to create HelloTask!\n");
-    }
+    g_bleTcb = osThreadNew((osThreadFunc_t)BLE_Task, NULL, &attr);
+
+    LOS_ASSERT_COND(g_bleTcb != NULL);
+
+    // if (osThreadNew((osThreadFunc_t)BLE_Task, NULL, &attr) == NULL) {
+    //     printf("[HelloDemo] Failed to create HelloTask!\n");
+    // }
 
     pwr_mgmt_mode_set(PMR_MGMT_SLEEP_MODE);
 }
